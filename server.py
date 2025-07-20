e409970862b32fa546f26a7cef79f178554df31d
nutthakitmuenphetnin <nutthakit2529@gmail.com>
2025-07-20 11:56:25 +0700

    server.py

--- server.py
+++ server.py
@@ -49,5 +49,7 @@ def serve_logs_file(filename):
 
 # 4. รัน Flask App
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
